c9a42d1069f8a4acd5e0774dc874f7e79f9226e2
parksil0 <parksil00@naver.com>
2022-07-17 00:15:34 +0900

    setting type to parameters

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,6 +1,9 @@
 import { startDevServer } from '@cypress/vite-dev-server';
 
-module.exports = (on, config) => {
+module.exports = (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions,
+) => {
   on('dev-server:start', async (options) => startDevServer({ options }));
 
   return config;
