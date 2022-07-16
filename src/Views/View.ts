b9d3a921ffcfe6d20c4d8bbbacc8b214b99dd6f3
parksil0 <parksil00@naver.com>
2022-07-16 16:27:05 +0900

    feat(method): addCustomEventListener 메서드 생성

--- src/Views/View.ts
+++ src/Views/View.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_EVENT_NAME } from '../constants/index.js';
 import { emit } from '../utils/dom.js';
 
 export default class View {
@@ -20,12 +21,22 @@ export default class View {
     return this;
   }
 
-  emit<T>(eventName: string, data: T) {
+  emitCustomEvent<T = unknown>(
+    eventName: keyof typeof CUSTOM_EVENT_NAME,
+    data: T,
+  ) {
     emit(this.element, eventName, data);
     return this;
   }
 
   addEventListener(eventName: string, handler: (e: Event) => void) {
+    this.element.addEventListener(eventName, handler);
+  }
+
+  addCustomEventListener(
+    eventName: keyof typeof CUSTOM_EVENT_NAME,
+    handler: (e: Event) => void,
+  ) {
     this.element.addEventListener(eventName, handler);
   }
 }
